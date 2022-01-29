cb0a7ce9c4711217968493e7eee3ed9eccc4046f
Yucel Sarikaya <berkaykata@gmail.com>
2022-01-29 21:40:27 +0300

    popup yapildi

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { GrFormClose } from "react-icons/gr";
+import ModalPopup from './ModalPopup';
+
+function App() {
+  
+  return (
+    <div >
+      <ModalPopup/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ModalPopup.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useState } from 'react';
+import Modal from 'react-modal/lib/components/Modal';
+
+export default function ModalPopup() {
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    return (
+        <div>
+            <button onClick={openModal}>Open Modal</button>
+
+
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Example Modal"
+                className="contentModal"
+            >
+                <div>
+                    <div className='header'>
+                        <div className='title'><strong>NASIL OYNANIR</strong></div>
+                        <div className='close'>
+                            <button onClick={closeModal}>
+                                x
+                            </button>
+                        </div>
+                    </div>
+                    <div><p><strong>WORDLE</strong>'i 6 denemede bulun.</p></div>
+                    <div><p>Her tahmin 5 harfli doğru bir kelime olmalıdır. Göndermek için enter'a basın.</p></div>
+                    <div><p>Her tahminden sonra kutucukların renkleri tahmininizin yakınlığına göre değişecektir.</p></div>
+                    <hr />
+                    <div><strong><p>Örnekler</p></strong></div>
+                    <div className='box green'>A</div>
+                    <div className='box'>B</div>
+                    <div className='box'>O</div>
+                    <div className='box'>N</div>
+                    <div className='box'>E</div>
+                    <div><p><strong>A</strong> harfi kelimede var ve doğru yerde.</p></div>
+                    <div className='box'>G</div>
+                    <div className='box yellow'>İ</div>
+                    <div className='box'>Z</div>
+                    <div className='box'>E</div>
+                    <div className='box'>M</div>
+                    <div><p><strong>İ</strong> harfi kelimede var fakat yanlış yerde.</p></div>
+                    <div className='box'>F</div>
+                    <div className='box'>A</div>
+                    <div className='box'>L</div>
+                    <div className='box'>E</div>
+                    <div className='box gray'>Z</div>
+                    <div><p><strong>Z</strong>  harfi kelimede yok. </p></div>
+                    <hr />
+                    <div><strong>Her gün yeni bir WORDLE gelir!</strong></div>
+                </div>
+
+            </Modal>
+        </div>
+    );
+}
